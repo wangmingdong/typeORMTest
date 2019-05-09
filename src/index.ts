fbbd74df6bf4eab1a7a49304a82b595e831cbb19
wangmingdong <540042281@qq.com>
2019-05-09 15:15:04 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+import { Photo } from "./entity/Photo";
+
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
+
+createConnection(/*...*/)
+  .then(async connection => {
+    let photo = new Photo();
+    photo.name = "Me and Bears";
+    photo.description = "I am near polar bears";
+    photo.filename = "photo-with-bears.jpg";
+    photo.views = 1;
+    photo.isPublished = true;
+
+    let photoRepository = connection.getRepository(Photo);
+
+    await photoRepository.save(photo);
+    console.log("Photo has been saved");
+
+    let savedPhotos = await photoRepository.find();
+    console.log("All photos from the db: ", savedPhotos);
+
+    let firstPhoto = await photoRepository.findOne(1);
+    console.log("First photo from the db: ", firstPhoto);
+
+    let meAndBearsPhoto = await photoRepository.findOne({ name: "Me and Bears" });
+    console.log("Me and Bears photo from the db: ", meAndBearsPhoto);
+
+    let allViewedPhotos = await photoRepository.find({ views: 1 });
+    console.log("All viewed photos: ", allViewedPhotos);
+
+    let allPublishedPhotos = await photoRepository.find({ isPublished: true });
+    console.log("All published photos: ", allPublishedPhotos);
+
+    let [allPhotos, photosCount] = await photoRepository.findAndCount();
+    console.log("All photos: ", allPhotos);
+    console.log("Photos count: ", photosCount);
+  })
+  .catch(error => console.log(error));
